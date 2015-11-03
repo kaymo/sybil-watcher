f87dc708f6acda692701be657ff97f688f10e92e
Kim <kim.mroz17@gmail.com>
2015-11-03 20:45:03 +0000

    Simplify the overlap proc

--- sense.py
+++ sense.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 import sys, time, datetime
 import sqlite3 as lite
 
+INTERVAL = 300
+
 # Connect to database
 con = None
 cur = None
@@ -24,9 +26,9 @@ def motion_detected(pin):
 
     if GPIO.input(pin):
         time_up = time.time()
-    else:
+    elif time_up > app_start:
         time_count += (time.time() - time_up)
-        time_up = 0
+        time_up = 0.0
 
 # Pi setup
 PIR_PIN = 7
@@ -41,23 +43,24 @@ try:
     
     # Infinite loop to summarise the motion detected
     while 1:
-        time.sleep(300)
+        time.sleep(INTERVAL)
         
         # If motion is currently detected then add to interval total
-        part = 0        
+        part = 0.0        
         if time_up > app_start:
             part = time.time() - time_up
+            time_up = time.time()
             time_count += part
         
         # Add to the db and print to console
-        if cur and time_count < 310.0:
+        if cur and (time_count < (INTERVAL+10)):
             cur.execute('INSERT INTO sybil (active) VALUES (?)', (round(time_count,3),) )
             con.commit()
 		
         print str(datetime.datetime.now()) + ": detected motion for " + str(round(time_count,3)) + "s"
         
         # Reset for the next interval
-        time_count = -part
+        time_count = 0.0
 
 except lite.Error, e:
     print "Error: {}".format(e.args[0])
